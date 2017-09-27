aec9c0ad1a90071ddb68daea17bc4c7d99680361
edeno <edeno@bu.edu>
2017-09-27 09:54:58 -0400

    Update for pip install

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 
 from setuptools import setup, find_packages
 
 setup(
     name='spectral',
     version='0.1.0.dev0',
+    license='GPL-3.0',
     description=('Frequency domain functional and directed'
                  'connectivity analysis tools for electrophysiological'
                  'data'),
     author='Eric Denovellis',
     author_email='edeno@bu.edu',
+    url='https://github.com/UriEdenLab/spectral',
     packages=find_packages(),
 )
