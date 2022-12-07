3861b77e638f98879b9a299a629f54ba2712a075
Eric Denovellis <edeno@bu.edu>
2022-12-07 12:48:48 -0500

    Remove path insert

--- docs/conf.py
+++ docs/conf.py
@@ -3,11 +3,7 @@
 
 import os
 import shutil
-import sys
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here.
-sys.path.insert(0, os.path.abspath("../"))
 
 # -- Project information -----------------------------------------------------
 
